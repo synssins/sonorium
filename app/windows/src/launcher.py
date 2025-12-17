8489a183b72d86f0ee69fdc2f5151de0f9d60419
Synssins <synssins@gmail.com>
2025-12-17 09:52:12 -0600

    Bump version to 0.1.8-alpha for new Tavern theme tracks

--- app/windows/src/launcher.py
+++ app/windows/src/launcher.py
@@ -39,7 +39,7 @@ from PyQt6.QtGui import QIcon, QPixmap, QAction, QDesktopServices, QFont, QTextC
 
 # Constants
 APP_NAME = "Sonorium"
-APP_VERSION = "0.1.7-alpha"
+APP_VERSION = "0.1.8-alpha"
 DEFAULT_PORT = 8008
 
 # Global logger instance
