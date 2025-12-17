53786b30d2d74cd3107b4107e043308682f88283
Synssins <synssins@gmail.com>
2025-12-17 14:18:12 -0600

    Bump version to 0.2.1-alpha

--- app/windows/src/launcher.py
+++ app/windows/src/launcher.py
@@ -39,7 +39,7 @@ from PyQt6.QtGui import QIcon, QPixmap, QAction, QDesktopServices, QFont, QTextC
 
 # Constants
 APP_NAME = "Sonorium"
-APP_VERSION = "0.2.0-alpha"
+APP_VERSION = "0.2.1-alpha"
 DEFAULT_PORT = 8008
 
 # Global logger instance
